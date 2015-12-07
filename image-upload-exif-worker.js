fcf5209e760d7a2696dcb6eced8cf72912c20b0d
Aleks Totic <a@totic.org>
2015-12-07 14:44:57 -0800

    docs

--- image-upload-exif-worker.js
+++ image-upload-exif-worker.js
@@ -1,29 +1,61 @@
 /**
- * Parses exif data from a file
- * Sends parsed event when complete
+ * Parses metadata from a file
  *
+ * Receives parsing requests with 'parse' event
+ * Sends metadata back with 'metadata' event
  */
-// postMessage("I\'m working before postMessage");
 
 importScripts('../exif-js/exif.js');
+// console.log("WORK STARTED");
 
-console.log("WORK STARTED");
+
+/**
+ * Parses File's metadata
+ * @event parse
+ * @param {string} ev.data.action -- 'parse', starts parsing the file
+ * @param {File} ev.data.file -- file to be parsed
+ * @param {string} ev.data.token --  token to pass back in 'metadata'
+ */
+
+/**
+ * Sends metadata back
+ * @event metadata
+ * @param {string} ev.data.action -- 'metadata'
+ * @param {string} ev.data.token -- token passed to 'parse'
+ * @param {string} ev.data.file  -- file metadata belong to
+ * @param {object} ev.data.metadata -- Metadata object
+ * @param {object} ev.data.metadata.exif -- Exif metadata object, has thumbnail
+ * @param {object} ev.data.metadata.iptc -- Iptc metadata
+ * @param {object} ev.data.metadata.xmp -- Xmp metadata
+ */
+
+/**
+ * @param {MessageEvent} ev -- message
+ * @param {Object} ev.data -- message's data
+ */
 onmessage = function (ev) {
+
   switch(ev.data.action) {
+    /**
+     * @event 'parse'
+     */
     case 'parse':
       EXIF.getData(ev.data.file, function() {
         postMessage({
-          action: 'parsed',
+          action: 'metadata',
           metadata: {
             exif: ev.data.file.exifdata,
             iptc: ev.data.file.iptcdata,
-            xmpdata: ev.data.file.xmpdata
+            xmp: ev.data.file.xmpdata
           },
           token: this.token,
           file: this.file });
       }.bind({file: ev.data.file, token: ev.data.token}));
-      break;
+    break;
+
     default:
       console.warn("unknown message", ev.data);
+      break;
   }
+
 };
